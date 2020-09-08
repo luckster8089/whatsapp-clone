4f5b7d0440d1db0d03eb78b81364506fe8147711
lucky-ly <luckster8089@gmail.com>
2020-09-08 12:03:54 -0500

    Added login and new id button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,18 @@
 import React, { useRef } from 'react'
-import { Container, Form } from 'react-bootstrap'
+import { Container, Form, Button } from 'react-bootstrap'
 
 export default function Login() {
     const idRef = useRef()
 
     return (
-        <Container className="align-items-center">
-        <Form>
+        <Container className="align-items-center d-flex" style={{ height:'100vh' }}>
+        <Form className="w-100">
             <Form.Group>
                 <Form.Label>Enter Your Id</Form.Label>
                 <Form.Control type="text" ref={idRef} required />
             </Form.Group>
+            <Button type="submit" className="mr-2">Login</Button>
+            <Button variant="secondary">Create a New Id</Button>
         </Form>
         </Container>
     )
