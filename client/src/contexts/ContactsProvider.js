704b0cdf92485398ad6e28bbdbe28365e044413b
luckster8089 <ly.lucky88@gmail.com>
2020-09-08 14:50:44 -0500

    Added ContactsProvider in contexts

---
+++ client/src/contexts/ContactsProvider.js
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react'
+
+const ContactsContext = React.createContext()
+
+export function ContactsProvider() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
