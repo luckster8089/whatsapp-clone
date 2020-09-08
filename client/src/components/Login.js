d47cf65e713049bbdb5d54bab0794f232fc8171a
lucky-ly <luckster8089@gmail.com>
2020-09-08 11:53:34 -0500

    Added login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { container } from 'react-bootstrap'
 
 export default function Login() {
     return (
-        <div>
+        <Container>
             
-        </div>
+        </Container>
     )
 }
 
