50556e582367ff22142944c2be0066fd672d5d31
lucky-ly <luckster8089@gmail.com>
2020-09-08 12:59:27 -0500

    Added Contacts/Conversation components and tabs to Sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tab, Nav } from 'react-bootstrap'
+import Conversations from './Conversations'
+import Contacts from './Contacts'
 
 const CONVERSATIONS_KEY = "conversations"
 const CONTACTS_KEY = "contacts"
 
 export default function Sidebar({ id }) {
+    const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
+    
     return (
         <div style={{ width: "250px" }} className="d-flex flex-column">
-            <Tab.Container>
-                <Nav>
+            <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
+                <Nav variant="tabs" className="justify-content-center">
                     <Nav.Item>
                         <Nav.Link eventKey={CONVERSATIONS_KEY}>
                             Conversations
@@ -20,6 +24,14 @@ export default function Sidebar({ id }) {
                         </Nav.Link>
                     </Nav.Item>
                 </Nav>
+                <Tab.Content>
+                    <Tab.Pane eventKey={CONVERSATIONS_KEY}>
+                        <Conversations />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey={CONTACTS_KEY}>
+                        <Contacts />
+                    </Tab.Pane>
+                </Tab.Content>
             </Tab.Container>
         </div>
     )
