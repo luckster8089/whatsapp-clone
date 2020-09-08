5ce278674bf4799234bdd969593f29b376c74c4a
luckster8089 <ly.lucky88@gmail.com>
2020-09-08 13:43:25 -0500

    Added newconversation/newcontact modal

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
-import { Tab, Nav } from 'react-bootstrap'
+import { Tab, Nav, Button, Modal } from 'react-bootstrap'
 import Conversations from './Conversations'
 import Contacts from './Contacts'
+import NewContactModal from './NewContactModal'
+import NewConversationModal from './NewConversationModal'
 
 const CONVERSATIONS_KEY = "conversations"
 const CONTACTS_KEY = "contacts"
 
 export default function Sidebar({ id }) {
     const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
+    const conversationsOpen = activeKey === CONVERSATIONS_KEY
 
     return (
         <div style={{ width: "250px" }} className="d-flex flex-column">
@@ -32,7 +35,20 @@ export default function Sidebar({ id }) {
                         <Contacts />
                     </Tab.Pane>
                 </Tab.Content>
+                <div className="p-2 border-top border-right small">
+                    Your Id: <span className="text-muted">{id}</span>
+                </div>
+                <Button className="rounded-0">
+                    New {conversationsOpen ? 'Conversation' : 'Contact'}
+                </Button>
             </Tab.Container>
+
+            <Modal>
+                {conversationsOpen ? 
+                    <NewConversationModal /> : 
+                    <NewContactModal /> 
+                }
+            </Modal>
         </div>
     )
 }
