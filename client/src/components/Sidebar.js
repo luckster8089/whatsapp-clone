d45189d5a08226db2688be577bbf63084bf65521
lucky-ly <luckster8089@gmail.com>
2020-09-08 12:49:20 -0500

    Added Conversations/Contacts to Nav

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { Tab, Nav } from 'react-bootstrap'
 
+const CONVERSATIONS_KEY = "conversations"
+const CONTACTS_KEY = "contacts"
+
 export default function Sidebar({ id }) {
     return (
-        <div>
-            <Tab.Container />
-            
+        <div style={{ width: "250px" }} className="d-flex flex-column">
+            <Tab.Container>
+                <Nav>
+                    <Nav.Item>
+                        <Nav.Link eventKey={CONVERSATIONS_KEY}>
+                            Conversations
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey={CONTACTS_KEY}>
+                            Contacts
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Tab.Container>
         </div>
     )
 }
