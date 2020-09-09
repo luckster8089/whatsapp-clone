c9e8101e5cd5f29d2cdc89ca5ae13cacf008e349
luckster8089 <ly.lucky88@gmail.com>
2020-09-09 14:24:22 -0500

    Added socket io to server

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { Form, InputGroup, Button } from 'react-bootstrap'
 import { useConversations } from '../contexts/ConversationsProvider'
 
 
 export default function OpenConversation() {
     const [text, setText] = useState('')
+    const setRef = useCallback(node => {
+        if (node) {
+        node.scrollIntoView({ smooth: true })
+        }
+    }, [])
     const { sendMessage, selectedConversation } = useConversations()
 
     function handleSubmit(e) {
@@ -20,19 +25,24 @@ export default function OpenConversation() {
     return (
         <div className="d-flex flex-column flex-grow-1">
             <div className="flex-grow-1 overflow-auto">
-                <div className="h-100 d-flex flex-column align-items-start justify-content-end px-3">
+                <div className="d-flex flex-column align-items-start justify-content-end px-3">
                     {selectedConversation.messages.map((message, index) => {
+                        const lastMessage = selectedConversation.messages.length - 1 === index
                         return (
-                            <div key={index} 
-                            className={`my-1 d-flex flex-column ${message.fromMe ? "align-self-end" : "" }`}>
+                            <div 
+                            ref={lastMessage ? setRef : null}
+                            key={index} 
+                            className={`my-1 d-flex flex-column ${message.fromMe ? "align-self-end align-items-end" : "align-items-start"}`}>
                                 <div 
-                                    className={`rounded px-2 py-1 ${message.fromMe ? 
-                                    "bg-primary text-white" : "border"}`}>
-                                    {message.text}
+                                className={`rounded px-2 py-1 ${message.fromMe ? 
+                                "bg-primary text-white" : "border"}`}>
+                                {message.text}
                                 </div>
                                 <div 
-                                    className={`text-muted small ${message.fromMe} ? 
-                                    "text-right" : "" `}>{message.fromMe ? "You" : message.senderName}</div>
+                                className={`text-muted small ${message.fromMe ? 
+                                "text-right" : ""}`}>
+                                {message.fromMe ? "You" : message.senderName}
+                                </div>
                             </div>
                         )
                     })}
@@ -59,3 +69,4 @@ export default function OpenConversation() {
         </div>
     )
 }
+
